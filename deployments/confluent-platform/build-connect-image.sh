b718f654cf850df4f083539b8b66ea1eb4c9a916
nh <Nadja.Hagen@novatec-gmbh.de>
2020-12-05 14:40:06 +0100

    Initial commit: Added Helm Charts for Confluent Platform and other showcase components.

---
+++ deployments/confluent-platform/build-connect-image.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+pushd . > /dev/null
+cd $(dirname ${BASH_SOURCE[0]})
+SCRIPT_DIR=$(pwd)
+popd > /dev/null
+
+docker build -t localhost:32777/cp-connect-emob:5.5.0 --build-arg CONNECTORS="confluentinc/kafka-connect-mqtt:1.3.0 confluentinc/kafka-connect-jdbc:5.5.0 mongodb/kafka-connect-mongodb:1.2.0 debezium/debezium-connector-mongodb:1.2.2" -f $SCRIPT_DIR/Dockerfile.connect $SCRIPT_DIR
